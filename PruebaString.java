092e9ba408301b6767550ce7b2d8b91c3c0a4b52
El tipazo <juanvera2001@hotmail.com>
2020-12-11 00:07:01 -0300

    Subiendo archivos

---
+++ PruebaString.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class PruebaString
+{
+   public static void main (String[]args)
+   {
+      String mens= "Hola esto es reso";
+      System.out.println(mens);
+   
+   
+   }
+
+}  
